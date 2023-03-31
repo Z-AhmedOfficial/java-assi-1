919a49a93049558357d614543c1eb39c41363fea
Z-AhmedOfficial <128061591+Z-AhmedOfficial@users.noreply.github.com>
2023-03-31 21:55:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+alert("WELCOME");
+var myname = "ZAIN AHMED HERE";
+console.log(myname);
+var x = 22;
+var y = 20;
+var mul = x * y;
+console.log(mul);
+var num = 1;
+num++;
+console.log(num);
+var newnum = 4;
+newnum--;
+console.log(newnum);
+var TeamsfName = prompt("Enter Favorite team's first name, Manchester");
+var TeamsLName = prompt("Enter Favorite team's last name,United");
+console.log(TeamsfName + " " + TeamsLName + " is my favorite team ");
+var num1 = 2;
+if(num1 == 2){
+    print("increment number", num1++);
+}
+else{
+    print("Decremnet number",num1--);
+}
+console.log(num1);
